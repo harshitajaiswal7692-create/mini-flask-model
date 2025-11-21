c1fc6978cdbe4a4786463ba9c605af0c1e1a6270
Harshita Jaiswal <harshitajaiswal7692@gmail.com>
2025-11-21 10:49:33 +0530

    Initial commit

---
+++ add.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Welcome to the Mini Flask Model!"
+
+@app.route('/add', methods=['POST'])
+def add_numbers():
+    data = request.get_json()
+    a = data.get("a", 0)
+    b = data.get("b", 0)
+    result = a + b
+    return jsonify({
+        "a": a,
+        "b": b,
+        "result": result
+    })
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
+
