cd032f4f52dcf6fec3a5e986ad68b79fe0848739
Harshita Jaiswal <harshitajaiswal7692@gmail.com>
2025-11-24 16:59:39 +0530

    change app.py

--- app.py
+++ app.py
@@ -2,24 +2,29 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
+# Home route
+@app.route("/", methods=["GET"])
 def home():
     return "Welcome to the Mini Flask Model!"
+
+# Flask status route
 @app.route("/flask", methods=["GET"])
-def home():
+def flask_status():
     return "Flask server running! Use POST /chat"
-@app.route('/add', methods=['POST'])
+
+# Add numbers route
+@app.route("/add", methods=["POST"])
 def add_numbers():
     data = request.get_json()
     a = data.get("a", 0)
     b = data.get("b", 0)
     result = a + b
+
     return jsonify({
         "a": a,
         "b": b,
         "result": result
     })
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-
